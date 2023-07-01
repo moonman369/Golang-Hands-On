554c4ec2c61fcaf64cb40452d141f74fc0102be9
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-07-01 12:53:11 +0530

    Add type casting, housekeeping and scope rule examples to variables-values sample

--- variables-values-identifiers/main.go
+++ variables-values-identifiers/main.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
+// VERY IMPORTANT NOTE:
+// The Short Declaration Operator i.e `:=` can only be used used inside a function scope
+
+// global := 90  ..........Invalid
+var global int = 90
+
 func main() {
+	fmt.Printf("global var = %v\n", global)
 
 	/**
 	* Go is a statically typed language but datatypes are figured out by the compiler automatically (hence they needn't be always specified)
 	*
 	 */
+
+	// VERY IMPORTANT NOTE:
+	// The Short Declaration Operator i.e `:=` can only be used used inside a function scope
 
 	a := 369
 	fmt.Printf("a => %d", a)
@@ -33,4 +43,26 @@ func main() {
 	str => %s
 	yn => %t
 	`, num, str, yn)
+
+	// GO IS A STSTICALLLY TYPED LANGUAGE!!!
+	v1 := 42.42
+	v2 := "psych"
+
+	fmt.Printf("%v is of type: %T\n", v1, v1)
+	fmt.Printf("%v is of type: %T\n", v2, v2)
+
+	// This is an invalid conversion because the types of v1 and v2 as evaluated by the compiler are float64 and string respectively and v2(string) can't be assigned a value of type float64
+
+	// v2 = v1 //Invalid
+
+	// Explicit Type Casting
+	var v3 float32 = 42.4242
+	v4 := float64(v3)
+	fmt.Printf("%v is of type: %T\n", v3, v3)
+	fmt.Printf("%v is of type: %T\n", v4, v4)
+
+	// Implicit Type Casting
+	var v5 float64 = float64(v3)
+	fmt.Printf("%v is of type: %T\n", v5, v5)
+
 }
