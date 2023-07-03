57c092f11348a3d8a6575348b01e8ac38217f6a5
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-07-03 11:28:02 +0530

    Minor change to bin-hex-dec handson

--- bin-dec-hex-handson/main.go
+++ bin-dec-hex-handson/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	a, b, c := 747, 911, 90210
 
-	fmt.Printf("dec(%v) = %d\n", a, a)
-	fmt.Printf("bin(%v) = %b\n", b, b)
-	fmt.Printf("dec(%v) = %#x\n", c, c)
+	fmt.Printf("dec(%v) = %d; \t bin(%v) = %b; \t hex(%v) = %#x\n", a, a, a, a, a, a)
+	fmt.Printf("dec(%v) = %d; \t bin(%v) = %b; \t hex(%v) = %#x\n", b, b, b, b, b, b)
+	fmt.Printf("dec(%v) = %d; \t bin(%v) = %b; \t hex(%v) = %#x\n", c, c, c, c, c, c)
 }
