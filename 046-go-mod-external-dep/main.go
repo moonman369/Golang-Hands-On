54f7b7d04b66625cd9a6c82d3e4c7b0d9829c1c3
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-07-04 23:11:21 +0530

    Try out latest version of `puppy`. Testes out go get .....@latest

--- 046-go-mod-external-dep/main.go
+++ 046-go-mod-external-dep/main.go
@@ -10,6 +10,10 @@ func main() {
 	var n int = 9
 	singleBark, nBarks, singleGrown, nGrown := puppy.Bark(), puppy.Barks(n), puppy.BigBark(), puppy.BigBarks(n)
 
+	fmt.Println("Puppy name before setting: ", puppy.PuppyName)
+	puppy.SetPuppyName("Cosmic Annihilation")
+	fmt.Println("Puppy name after setting: ", puppy.PuppyName)
+
 	fmt.Printf("`puppy.Bark()` call returned ==> %v\n`puppy.Barks(n int)` call returned ==> %v\n`puppy.BigBark()` call returned ==> %v\n`puppy.BigBarks(n int)` call returned ==> %v\n", singleBark, nBarks, singleGrown, nGrown)
 
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module goHandsOn
 
 go 1.20
 
-require github.com/moonman369/Puppy-Go-Dependency v0.0.0-20230704133241-e8a2a3c6d353
+require github.com/moonman369/Puppy-Go-Dependency v0.0.2
 
 require github.com/moonman369/Dog-Go-Depedency v0.0.0-20230704132511-4e0030efa578 // indirect
