e7f44af2eb42366e2476d186dfff823bcfd96280
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-07-05 11:10:11 +0530

    Update dependent code to use `puppy@v0.0.4` features

--- 046-go-mod-external-dep/main.go
+++ 046-go-mod-external-dep/main.go
@@ -13,10 +13,17 @@ func main() {
 	fmt.Println("Puppy name before setting: ", puppy.PuppyName)
 	puppy.SetPuppyName("Cosmic Annihilation")
 	fmt.Println("Puppy name after setting: ", puppy.PuppyName)
+	fmt.Println()
 
 	fmt.Println("Puppy age before setting: ", puppy.PuppyAge)
-	puppy.SetPuppyAge(5)
+	puppy.SetPuppyAge(int8(n))
 	fmt.Println("Puppy age after setting: ", puppy.PuppyAge)
+	fmt.Println()
+
+	fmt.Println("Puppy favorite snack before setting: ", puppy.FavSnack)
+	puppy.SetFavSnack("5 Year olds")
+	fmt.Println("Puppy favorite snack after setting: ", puppy.FavSnack)
+	fmt.Println()
 
 	fmt.Printf("`puppy.Bark()` call returned ==> %v\n`puppy.Barks(n int)` call returned ==> %v\n`puppy.BigBark()` call returned ==> %v\n`puppy.BigBarks(n int)` call returned ==> %v\n", singleBark, nBarks, singleGrown, nGrown)
 
