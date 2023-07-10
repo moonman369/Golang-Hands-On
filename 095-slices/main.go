f81221d7e0744facf7e3ab0ff133b6bc3ec125b5
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-07-10 23:35:47 +0530

    Add Appending and slicing slice demos to 095-slices

--- 095-slices/main.go
+++ 095-slices/main.go
@@ -9,4 +9,17 @@ func main() {
 
 	var s2 []int
 	fmt.Println(s2)
+
+	// Append to slice using append()
+	xi := []int{23, 54, 42, 76, 24, 87}
+	fmt.Printf("Before appending: %v\n", xi)
+	// Using variadic param to append any arbitrary number of similar type values
+	xi = append(xi, 0, 1, 345, 353)
+	fmt.Printf("After appending: %v\n", xi)
+
+	// Slicing a slice
+	// Works like python slicing x[incl:excl]
+	xi1, xi2, xi3 := xi[3:8], xi[3:], xi[:8]
+
+	fmt.Printf("xi[3:8] = %#v\nxi[3:] = %#v\nxi[:8] = %#v\n", xi1, xi2, xi3)
 }
