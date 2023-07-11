2b1275bb0331ab7b5693508528145e00cf09ad06
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-07-11 10:25:22 +0530

    Add deleting from slice and `make()` demo to 095 slices

--- 095-slices/main.go
+++ 095-slices/main.go
@@ -22,4 +22,26 @@ func main() {
 	xi1, xi2, xi3 := xi[3:8], xi[3:], xi[:8]
 
 	fmt.Printf("xi[3:8] = %#v\nxi[3:] = %#v\nxi[:8] = %#v\n", xi1, xi2, xi3)
+
+	// Deleting from a slice
+	xi4 := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	// Using append
+	// for deleting item at nth index
+	// xi = append(xi[:n], xi[n+1:])
+	fmt.Printf("Before deletion of val at idx 4: %#v\n", xi4)
+	xi4 = append(xi4[:4], xi4[5:]...) // Destructuring a slice to unpack the elements. Like JS but the ... is added after the slice
+	fmt.Printf("After deletion of val at idx 4:  %#v\n", xi4)
+
+	// Creating a slice using `make()` function
+	xi5 := make([]int, 0, 10) // (type, init len(size), max length(capacity)) : the max len parameter saves the need to expand the underlying array everytime an overflow element is added to the slice. This saves compile time. If the max len of a slice is known, it is advisable to use make() fn
+	fmt.Println(len(xi5))     // returns current length
+	fmt.Println(cap(xi5))     // cap returns capacity of the underlying array
+	for i := 0; i < 21; i++ {
+		xi5 = append(xi5, i)
+		fmt.Println("Len =", len(xi5))
+		fmt.Println("Cap =", cap(xi5))
+	}
+
+	// Multi-dimensional slices
+
 }
