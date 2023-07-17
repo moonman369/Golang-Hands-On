292b4470ee9b24228f5674c303d422a101b87515
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-07-17 10:51:11 +0530

    0123: Anonymous structs

--- 0122-structs/main.go
+++ 0122-structs/main.go
@@ -9,6 +9,7 @@ type person struct {
 	drinks    bool
 }
 
+// Embedded
 type secretAgent struct {
 	person
 	ltk bool
@@ -49,5 +50,32 @@ func main() {
 	// Automatically decides even if proper tree is not mentioned. Might cause collision if same fieldname is encountered. Fields from the inner structs get promoted to the outher structs
 	fmt.Printf("%v, %v, %v years. Drinks: %v. License To Kill: %v\n", sa1.lastName, sa1.firstName, sa1.age, sa1.drinks, sa1.ltk)
 	fmt.Printf("%v, %v, %v years. Drinks: %v. License To Kill: %v\n", sa1.person.lastName, sa1.person.firstName, sa1.person.age, sa1.person.drinks, sa1.ltk)
+
+	// Anonymous  structs
+	// We can skip the struct declaration and straight up create a struct type variable while declaring the struct type in the same statement. This is great for a one time use struct and it required no name for that particular type
+	// Normal struct decl: s := structType {
+	// 	field1: val1,
+	// 	.....
+	// }
+
+	// Anonymous struct decl:
+	// s := struct {
+	// 	field1 type1
+	// 	....
+	// } {
+	// 	field1: value1,
+	// 	...
+	// }
+
+	car1 := struct {
+		company  string
+		model    string
+		topSpeed int
+	}{
+		company:  "Hyundai",
+		model:    "Verna",
+		topSpeed: 180, // If a field is skipped, the default/zero value is assigned to field
+	}
+	fmt.Printf("%#v\n", car1)
 
 }
